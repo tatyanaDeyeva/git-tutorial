68691a57d75a01ff290b1d50f9f94279bf2aa50d
mikhail_deev <mdeev@neoflex.ru>
2021-09-18 19:09:04 +0300

    Merge conflict from hello-branch

--- src/GitTutorial.java
+++ src/GitTutorial.java
@@ -1,7 +1,7 @@
 public class GitTutorial {
     public static void main(String[] args) {
         System.out.println("Hello, Git!"+1);
-
+        System.out.println("Merge conflict");
         System.out.println("Hello, Git, from hello-branch.");
     }
 }
